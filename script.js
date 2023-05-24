010468ea51e9a87dc56b693ee68f28f56f3a8f5a
roland444 <123905024+roland444@users.noreply.github.com>
2023-05-24 23:30:36 +0200

    Added selectOption

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const _options = document.querySelector(".quiz-options");
 const _correctScore = document.getElementById("correct-score");
 const _totalQuestion = document.getElementById("total-question");
 
-//let correctAnswer = "";
+let correctAnswer = "";
 let correctScore = 0;
 let askedCount = 0;
 let totalQuestion = 10;
 
-window.onload = function() {
+
+document.addEventListener("DOMContentLoaded", () => {
     loadQuestion();
-    //_totalQuestion.textContent = totalQuestion;
-    //_correctScore.textContent = correctScore;
-}
+    _totalQuestion.textContent = totalQuestion;
+    _correctScore.textContent = correctScore;
+})
 
 async function loadQuestion() {
     const response = await fetch("https://the-trivia-api.com/v2/questions");
@@ -32,5 +33,21 @@ function showQuestion(data) {
             <li> <span> ${option} </span> </li>
         `).join("")}
     `;
+
+    selectOption();
 }
+
+function selectOption() {
+    _options.querySelectorAll("li").forEach((option) => {
+        option.addEventListener("click", () => {
+            if (_options.querySelector(".selected")) {
+                const activeOption = _options.querySelector(".selected");
+                activeOption.classList.remove("selected");
+            }
+            option.classList.add("selected")
+        })
+    })
+}
+
+
 
