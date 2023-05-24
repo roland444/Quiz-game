9f06b45486b11cc04c13186a882cc01ec8a0628b
roland444 <123905024+roland444@users.noreply.github.com>
2023-05-24 23:21:32 +0200

    Fixed optionsList map func

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const _question = document.getElementById("question");
-const _option = document.querySelectorAll("quiz-options");
+const _options = document.querySelector(".quiz-options");
 const _correctScore = document.getElementById("correct-score");
 const _totalQuestion = document.getElementById("total-question");
 
@@ -8,29 +8,28 @@ let correctScore = 0;
 let askedCount = 0;
 let totalQuestion = 10;
 
-window.onload = function () {
+window.onload = function() {
     loadQuestion();
-    _totalQuestion.textContent = totalQuestion;
-    _correctScore.textContent = correctScore;
+    //_totalQuestion.textContent = totalQuestion;
+    //_correctScore.textContent = correctScore;
 }
 
 async function loadQuestion() {
-    const APIUrl = "https://the-trivia-api.com/v2/questions";
-    const result = await fetch(`${APIUrl}`);
-    const data = await result.json();
+    const response = await fetch("https://the-trivia-api.com/v2/questions");
+    const data = await response.json();
     showQuestion(data[0]);
 }
 
 function showQuestion(data) {
     let correctAnswer = data.correctAnswer;
-    let incorrectAnswers = data.incorrectAnswers;
-    let optionsList = incorrectAnswers;
-    optionsList.splice(Math.floor(Math.random() * (incorrectAnswers.length + 1)), 0, correctAnswer);
+    let incorrectAnswer = data.incorrectAnswers;
+    let optionsList = incorrectAnswer;
+    optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
     
     _question.innerHTML = `${data.question.text}`;
-    _option.innerHTML = `
-        ${optionsList.map((_option) => `
-            <li><span>${_option}</span></li>
+    _options.innerHTML = `
+        ${optionsList.map((option) => `
+            <li> <span> ${option} </span> </li>
         `).join("")}
     `;
 }
